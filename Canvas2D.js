87dd0d75bc840d72d6b13471a31bab4e451de81a
I <i@Is-MacBook-Pro.local>
2018-03-03 16:54:36 +0200

    Deleted unnecessary function from Canvas2D class.

--- Canvas2D.js
+++ Canvas2D.js
@@ -92,26 +92,6 @@ Canvas2D_Singleton.prototype.drawImage = function (sprite, position, rotation, s
     this._canvasContext.restore();
 };
 
-function toUTF16(codePoint) {
-    var TEN_BITS = parseInt('1111111111', 2);
-    function u(codeUnit) {
-        return '\\u'+codeUnit.toString(16).toUpperCase();
-    }
-
-    if (codePoint <= 0xFFFF) {
-        return u(codePoint);
-    }
-    codePoint -= 0x10000;
-
-    // Shift right to get to most significant 10 bits
-    var leadSurrogate = 0xD800 + (codePoint >> 10);
-
-    // Mask to get least significant 10 bits
-    var tailSurrogate = 0xDC00 + (codePoint & TEN_BITS);
-
-    return u(leadSurrogate) + u(tailSurrogate);
-}
-
 Canvas2D_Singleton.prototype.drawText = function (text, position, origin, color, textAlign, fontname, fontsize) {
     var canvasScale = this.scale;
 
